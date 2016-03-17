38a9340a1f78e1b243114d7d76798928aa894bd1
martin <tlkuo.frb@gmail.com>
2016-03-17 21:21:12 +0800

    Add button exercise

---
+++ button/BtnViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BtnViewController.swift
+//  button
+//
+//  Created by martin on 2016/3/17.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class BtnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
