7c271dec19badcd89a2ed7d0dafc340861956467
martin <tlkuo.frb@gmail.com>
2016-03-21 14:13:18 +0800

    Add ebook exercise

---
+++ ebook/BookViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ebook
+//
+//  Created by martin on 2016/3/18.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class BookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
