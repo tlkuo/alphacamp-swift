52b3b8b580dc22a224a34e9d22bd9888f26e0638
martin <tlkuo.frb@gmail.com>
2016-04-01 18:57:03 +0800

    Add alphacamp app main layout

---
+++ alphacamp/ACEventDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ACEventDetailViewController.swift
+//  alphacamp-swift
+//
+//  Created by martin on 2016/4/1.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class ACEventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
