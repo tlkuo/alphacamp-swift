aaa3b586656a28a81cd529e3031805c4d9ddb8b2
martin <tlkuo.frb@gmail.com>
2016-04-01 14:21:11 +0800

    Add local notification exercise

---
+++ notification/NotifyViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotifyViewController.swift
+//  notification
+//
+//  Created by martin on 2016/4/1.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class NotifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
